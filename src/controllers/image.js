3aefed556025e768f6d2148c31d231230af756db
Maylen Nunez <maylennunez@hotmail.com>
2020-04-12 18:37:53 -0400

    ctrl.create update

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const {randomNumber} = require('../helpers/libs');
+const fs = require('fs-extra');
 const ctrl = {};
 
 ctrl.index = (req, res) => {
@@ -13,12 +14,11 @@ console.log(imgUrl);
 const imageTempPath = req.file.path;                  // get image location
 const ext = path.extname(req.file.originalname).toLocaleLowerCase();          // to get extension 
 const targetPath = path.resolve('src/public/upload/${imgUrl}${ext}')
-res.send('works!');
 
 if (ext === '.png' || ext === '.jpg' || ext === '.gif') {
-
+    await fs.rename(imageTempPath, targetPath);       // move image from imageTempPath to targetPath
 }
-   
+res.send('works!');
 };
 
 

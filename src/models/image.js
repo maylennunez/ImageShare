72e59a22a4795c2ec28df9924d529fafe16126c8
Maylen Nunez <maylennunez@hotmail.com>
2020-04-13 21:53:37 -0400

    schema

--- src/models/image.js
+++ src/models/image.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+
+ const imageSchema = new schema({
+    title: {type : String},
+    description: {type: String},
+    filename : {type:String},
+    views: {type : Number, default: 0},
+    likes: {type: Number, default: 0},
+    timestamp: {type: Date, default: Date.now }
+
+})
+
+
+
+module.exports = mongoose.model('Image', imageSchema)

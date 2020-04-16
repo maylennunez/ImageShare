fa90213ad9608bd0d0c42c15ed5d1ae471a46f2e
Maylen Nunez <maylennunez@hotmail.com>
2020-04-15 22:25:01 -0400

    test

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const {database} = require('./keys'); 
 
-mongoose.connect(database.URI, {  useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(database.URI, { useNewUrlParser: true
 })
 .then(db => console.log('db is connected'))
 .catch(err => console.log(err));

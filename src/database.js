cbe80cc822d97f88c3d3c957ffe98f7d0933be06
Maylen Nunez <maylennunez@hotmail.com>
2020-04-18 23:16:37 -0400

    node_modules

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+const MongoClient = require('mongodb').MongoClient
 const {database} = require('./keys'); 
 
-mongoose.connect(database.URI, { useNewUrlParser: true
+MongoClient.connect("database.URI",
+     { useNewUrlParser: true, useUnifiedTopology: true  
 })
 .then(db => console.log('db is connected'))
 .catch(err => console.log(err));

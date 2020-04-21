d5f6736311688688d014e242928fe06ee8e85c45
Maylen Nunez <maylennunez@hotmail.com>
2020-04-20 23:04:48 -0400

    changes

--- src/server/config.js
+++ src/server/config.js
@@ -13,7 +13,7 @@ module.exports = app => {
     //Settings
    
     app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(__dirname, '../views'))
+    app.set('view', path.join(__dirname, '../views'))
     app.engine('.hbs', exphbs({
         defaultLayout: 'main',
         partialsDir: path.join(app.get('views'), 'partials'),
@@ -21,7 +21,7 @@ module.exports = app => {
         extname: '.hbs',
         helpers: require('./helpers')
     }));
-    app.set('views engine', 'hbs');
+    app.set('view engine', 'hbs');
 
 
 

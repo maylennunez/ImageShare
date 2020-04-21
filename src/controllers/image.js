edbd5ce3a0ba43edad2d3f154e0d5efff5786273
Maylen Nunez <maylennunez@hotmail.com>
2020-04-20 21:16:20 -0400

    update views

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -3,23 +3,33 @@ const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
 const ctrl = {};
 const md5 = require('md5');
-const { Image , Comment } = require('../models/index');
+const { Image, Comment } = require('../models/index');
 
 
 
 
 ctrl.index = async (req, res) => {
     // console.log(req.params.image_id)
-    const image = await Image.findOne({filename: {$regex: req.params.image_id}})
-const comments = await Comment.find({image_id: image._id});
-res.render('image', {image, comments});
+    const viewModel = {image: {}, comments: {}};
+
+    const image = await Image.findOne({ filename: { $regex: req.params.image_id } })
+    if (image) {                                             // image validation
+    image.views = image.views + 1;
+    viewModel.image = image;
+    await image.save();
+    const comments = await Comment.find({ image_id: image._id });
+    viewModel.comments = comments;
+    res.render('image', {image, viewModel });
+    } else {
+        res.redirect('/');
+    }
 };
 
-ctrl.create =  (req, res) => {
+ctrl.create = (req, res) => {
     // console.log(req.file);
-     const saveImage = async () => {
+    const saveImage = async () => {
 
-        const imgUrl = randomNumber();                                                     
+        const imgUrl = randomNumber();
         const images = await Image.find({ filename: imgUrl });            // randon number validation
         if (images.length > 0) {
             saveImage();
@@ -44,27 +54,27 @@ ctrl.create =  (req, res) => {
             }
             // console.log(newImg)
             // }
-            
+
         };
     }
-    
+
 };
 
 
 
 ctrl.like = (req, res) => {
-   
+
 };
 
 ctrl.comment = async (req, res) => {
-  //  console.log(req.body)
-   const image = await Image.findOne({filename: {$regex: req.params.image_id}})
+    //  console.log(req.body)
+    const image = await Image.findOne({ filename: { $regex: req.params.image_id } })
     if (image) {
-     const newComment = new Comment(req.body);
-     newComment.gravatar = md5(newComment.email);
-     newComment.image_id = image._id;
-    await newComment.save();
-   // console.log(newComment)
+        const newComment = new Comment(req.body);
+        newComment.gravatar = md5(newComment.email);
+        newComment.image_id = image._id;
+        await newComment.save();
+        // console.log(newComment)
     }
     res.redirect('/images' + image.uniqueId)
 };
